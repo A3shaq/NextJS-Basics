23edf4975d4d5a52b75b4614ed41c724b93e1433
a3shaq <arshaqshakeel3@gmail.com>
2023-01-27 17:00:07 +0500

    add icons jsx

--- src/components/events/EventItem.js
+++ src/components/events/EventItem.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
+import DateIcon from "../icons/date-icon";
 import Button from "../ui/button";
-import classes from "./event-item.module.css"
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
   const { id, url, title, location, date } = props.item;
@@ -12,21 +15,27 @@ function EventItem(props) {
   });
   return (
     <li className={classes.item}>
-      <img alt="No Image Founded" src={url} width="200" height={"100"}  />
+      <img alt="No Image Founded" src={url} width="200" height={"100"} />
 
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{location}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          {/* <Link href={`/events/${id}`}>Explore Event</Link> */}
-          <Button link={`/events/${id}`}>Explore Events</Button>
+          <Button link={`/events/${id}`}>
+            <span>Explore Events</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
